be6ff5eae71866305f923ddf6cf573be6ad76587
Daniel Wykerd <daniel@wykerd.dev>
2024-09-10 13:51:45 +0200

    feat(EventLog): use virtual scrolling for performance

--- src/components/EventLog.tsx
+++ src/components/EventLog.tsx
@@ -1,10 +1,11 @@
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Separator } from "@/components/ui/separator"
 import { Antenna, CirclePlus, Compass, Locate, LocateFixed, LocateOff, LucideIcon, Radio, Unplug } from "lucide-react"
-import { useMemo } from "react"
+import { useMemo, useRef } from "react"
 import { Button } from "./ui/button"
 import { useInspector } from "@/lib/Inspector"
 import { ClientJoinMessage, ClientLeaveMessage, ClientMoveMessage, DisseminationMessage, InfoMessage, PubMessage, SubscriptionDeleteMessage, SubscriptionNewMessage, TypedMessage } from "@/lib/messages"
+import { useVirtualizer } from '@tanstack/react-virtual';
 
 function EventLogEntry({
     log, icon: Icon, timestamp, children
@@ -23,7 +24,7 @@ function EventLogEntry({
     }, [timestamp]);
 
     return <>
-        <div className="text-xs flex flex-row gap-2 items-center justify-between">
+        <div className="text-xs flex flex-row gap-2 items-center justify-between py-2 border-b border-border">
             <div className="flex flex-row gap-2 items-center">
                 <Icon size={16} />
                 <span>
@@ -39,11 +40,10 @@ function EventLogEntry({
                 </span>
             </div>
         </div>
-        <Separator className="my-2" />
     </>
 }
 
-function toMessage(event: TypedMessage)  {
+function toMessage(event: TypedMessage) {
     switch (event.type) {
         case 'client-join':
             {
@@ -91,7 +91,7 @@ function toMessage(event: TypedMessage)  {
                 const dis = event as DisseminationMessage;
                 return `Matcher #${dis.sub.hostID} disseminated message on channel '${dis.sub.channel}' to '${dis.sub.subID}'`
             }
-    
+
         default:
             return "Unknown event";
             break;
@@ -102,29 +102,57 @@ function toMessage(event: TypedMessage)  {
 export default function EventLog() {
     const inspector = useInspector();
 
+    const parentRef = useRef<HTMLDivElement>(null!);
+
+    const rowVirtualizer = useVirtualizer({
+        count: inspector.state.events.length,
+        getScrollElement: () => parentRef.current,
+        estimateSize: () => 33,
+        overscan: 10,
+    });
+
     return (
-        <ScrollArea className="max-h-full h-full">
-            <div className="px-4">
-                <div className="py-4 sticky top-0 bg-white border-b border-border mb-2">
-                    <h4 className="text-sm font-medium leading-none">Events</h4>
-                </div>
-                {
-                    inspector.state.events.map((event, index) => <EventLogEntry 
-                        key={index}
-                        timestamp={event.time.getTime()}
-                        icon={
-                            event.type === "client-join" ? CirclePlus :
-                            event.type === "client-leave" ? Unplug :
-                            event.type === "client-move" ? Compass :
-                            event.type === "pub" ? Radio :
-                            event.type === "sub-new" ? Locate :
-                            event.type === "sub-delete" ? LocateOff :
-                            Antenna
-                        }
-                        log={toMessage(event)}
-                    />)
-                }
+        <div className="flex flex-col h-full">
+            <div className="py-4 bg-white border-b border-border mx-4">
+                <h4 className="text-sm font-medium leading-none">Events</h4>
             </div>
-        </ScrollArea>
+            <div className="px-4 overflow-auto grow" ref={parentRef}>
+                <div
+                    style={{
+                        height: `${rowVirtualizer.getTotalSize()}px`,
+                        width: '100%',
+                        position: 'relative',
+                    }}
+                >
+                    {rowVirtualizer.getVirtualItems().map((virtualItem) => (
+                        <div
+                            key={virtualItem.key}
+                            style={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                width: '100%',
+                                height: `${virtualItem.size}px`,
+                                transform: `translateY(${virtualItem.start}px)`,
+                            }}
+                        >
+                            <EventLogEntry
+                                log={toMessage(inspector.state.events[virtualItem.index])}
+                                timestamp={inspector.state.events[virtualItem.index].time.getTime()}
+                                icon={
+                                    inspector.state.events[virtualItem.index].type === "client-join" ? CirclePlus :
+                                    inspector.state.events[virtualItem.index].type === "client-leave" ? Unplug :
+                                    inspector.state.events[virtualItem.index].type === "client-move" ? Compass :
+                                    inspector.state.events[virtualItem.index].type === "pub" ? Radio :
+                                    inspector.state.events[virtualItem.index].type === "sub-new" ? Locate :
+                                    inspector.state.events[virtualItem.index].type === "sub-delete" ? LocateOff :
+                                    Antenna
+                                }
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
     )
 }
